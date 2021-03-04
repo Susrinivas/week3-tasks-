bc39417cc5dcb2d69c1f74b22c23758a8f2ec115
Susrinivas <79036336+Susrinivas@users.noreply.github.com>
2021-03-04 21:48:18 +0530

    Delete task4.js

--- task4.js
+++
@@ -1,9 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('Get','https://restcountries.eu/rest/v2/all');
-request.send();
-request.onload = function(){
-    var countrydata = JSON.parse(this.response);
-    
-    let result = countrydata.reduce((result,item) => result+item.population, 0);
-    console.log(result.toLocaleString());
-}
